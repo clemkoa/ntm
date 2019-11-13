a2784307299bec47bb65c88bf2190d7b02c2722b
clemkoa <clement.joudet@gmail.com>
2019-11-13 21:59:36 +0100

    feat: simple linear network

---
+++ main.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+class NTM(nn.Module):
+    def __init__(self):
+        super(NTM, self).__init__()
+        self.layer = nn.Linear(6, 6)
+
+    def forward(self, x):
+        return self.layer(x)
+
+
+input = torch.tensor([[0.0, 1.0, 0, 1, 1, 0]])
+target = torch.tensor([[0.0, 1.0, 0, 1, 1, 0]])
+
+model = NTM()
+optimizer = optim.Adam(model.parameters(), lr = 0.001)
+
+for i in range(1000):
+    optimizer.zero_grad()
+    output = model(input)
+    loss = F.mse_loss(output, target)
+    print(loss)
+    loss.backward()
+    optimizer.step()

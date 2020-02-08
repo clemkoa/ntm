92362a1ee04834a1ff842fcf36be6942ed40183a
clement.joudet <clement.joudet@inventia.life>
2020-02-08 18:31:14 +1100

    feat: adding dummy head state

--- main.py
+++ main.py
@@ -12,10 +12,14 @@ class NTM(nn.Module):
         self.write_head = WriteHead(self.memory)
         self.fc = nn.Linear(6 + 20, 6)
 
-    def forward(self, x):
-        read = self.read_head(self.controller(x))
-        fc_input = torch.cat((x, read), 1)
-        return F.sigmoid(self.fc(fc_input))
+    def forward(self, x, previous_state):
+        previous_read_head_state, previous_write_head_state = previous_state
+        controller_output = self.controller(x)
+        read_output, read_state = self.read_head(controller_output, previous_read_head_state)
+
+        fc_input = torch.cat((controller_output, read_output), 1)
+        state = (read_state, previous_read_head_state)
+        return F.sigmoid(self.fc(fc_input)), state
 
 class Controller(nn.Module):
     def __init__(self):
@@ -31,8 +35,8 @@ class ReadHead(nn.Module):
         self.weights = nn.Linear(6, 10)
         self.memory = memory
 
-    def forward(self, x):
-        return torch.matmul(self.weights(x), self.memory)
+    def forward(self, x, previous_state):
+        return torch.matmul(self.weights(x), self.memory), previous_state
 
 class WriteHead(nn.Module):
     def __init__(self, memory):
@@ -46,12 +50,16 @@ class WriteHead(nn.Module):
 input = torch.tensor([[0.0, 1.0, 0, 1, 1, 0]])
 target = torch.tensor([[0.0, 1.0, 0, 1, 1, 0]])
 
+initial_read_head_weights = torch.ones((6, 1))
+initial_write_head_weights = torch.ones((6, 1))
+state = (initial_read_head_weights, initial_write_head_weights)
+
 model = NTM()
 optimizer = optim.Adam(model.parameters(), lr = 0.001)
 
 for i in range(1000):
     optimizer.zero_grad()
-    output = model(input)
+    output, state = model(input, state)
     loss = F.mse_loss(output, target)
     print(loss)
     loss.backward()

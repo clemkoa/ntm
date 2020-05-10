8f05b2fd6f3ebb8e7bed0ac4ca354c946c6c9008
Clement Joudet <clement.joudet@gmail.com>
2020-05-10 15:52:02 +1000

    feat: read output as controller input

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from utils import circular_convolution
 class NTM(nn.Module):
     def __init__(self, vector_length, hidden_size, memory_size):
         super(NTM, self).__init__()
-        self.controller = Controller(vector_length, hidden_size)
+        self.controller = Controller(vector_length + memory_size[1], hidden_size)
         self.memory = Memory(memory_size)
         self.read_head = ReadHead(self.memory, hidden_size)
         self.write_head = WriteHead(self.memory, hidden_size)
         self.fc = nn.Linear(hidden_size + memory_size[1], vector_length)
 
     def forward(self, x, previous_state):
-        previous_read_head_state, previous_write_head_state, previous_controller_state = previous_state
-        controller_output, controller_state = self.controller(x, previous_controller_state)
+        previous_read, previous_read_head_state, previous_write_head_state, previous_controller_state = previous_state
+        controller_input = torch.cat([x, previous_read], dim=1)
+        controller_output, controller_state = self.controller(controller_input, previous_controller_state)
         # Read
         read_head_output, read_head_state = self.read_head(controller_output, previous_read_head_state)
-
         # Write
         write_head_state = self.write_head(controller_output, previous_read_head_state)
         fc_input = torch.cat((controller_output, read_head_output), dim=1)
-        state = (read_head_state, write_head_state, controller_state)
+        state = (read_head_output, read_head_state, write_head_state, controller_state)
         return F.sigmoid(self.fc(fc_input)), state
 
 
@@ -142,7 +142,7 @@ hidden_layer_size = 8
 sequence_length = 2
 
 model = NTM(vector_length, hidden_layer_size, memory_size)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.005)
 
 feedback_frequence = 100
 total_loss = []
@@ -156,9 +156,10 @@ model_path = 'models/copy.pt'
 initial_read_head_weights = torch.ones((1, memory_size[0])) / memory_size[0]
 initial_write_head_weights = torch.ones((1, memory_size[0])) / memory_size[0]
 initial_controller_weights = (torch.ones((1, 1, hidden_layer_size)) / hidden_layer_size, torch.ones((1, 1, hidden_layer_size)) / hidden_layer_size)
+initial_read = torch.ones((1, memory_size[1])) / memory_size[1]
 for i in range(100000):
     input, target = get_training_sequence(sequence_length, vector_length)
-    state = (initial_read_head_weights, initial_write_head_weights, initial_controller_weights)
+    state = (initial_read, initial_read_head_weights, initial_write_head_weights, initial_controller_weights)
     optimizer.zero_grad()
     for vector in input:
         output, state = model(vector, state)

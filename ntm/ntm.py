99e0c0b7f9efd402089ab49da0cd4de69b57d36c
Clement Joudet <clement.joudet@gmail.com>
2020-05-20 20:31:09 +1000

    feat: feed-forward controller

--- ntm/ntm.py
+++ ntm/ntm.py
@@ -19,20 +19,19 @@ class NTM(nn.Module):
 
     def get_initial_state(self):
         self.memory.reset()
-        controller_state = self.controller.get_initial_state()
         read = self.memory.get_initial_state()
         read_head_state = self.read_head.get_initial_state()
         write_head_state = self.write_head.get_initial_state()
-        return (read, read_head_state, write_head_state, controller_state)
+        return (read, read_head_state, write_head_state)
 
     def forward(self, x, previous_state):
-        previous_read, previous_read_head_state, previous_write_head_state, previous_controller_state = previous_state
+        previous_read, previous_read_head_state, previous_write_head_state = previous_state
         controller_input = torch.cat([x, previous_read], dim=1)
-        controller_output, controller_state = self.controller(controller_input, previous_controller_state)
+        controller_output = self.controller(controller_input)
         # Read
         read_head_output, read_head_state = self.read_head(controller_output, previous_read_head_state)
         # Write
         write_head_state = self.write_head(controller_output, previous_read_head_state)
         fc_input = torch.cat((controller_output, read_head_output), dim=1)
-        state = (read_head_output, read_head_state, write_head_state, controller_state)
+        state = (read_head_output, read_head_state, write_head_state)
         return F.sigmoid(self.fc(fc_input)), state
